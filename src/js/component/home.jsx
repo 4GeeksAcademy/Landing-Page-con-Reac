7634f354eea6d461d0ccead72c14f71a87c18bc6
nob86 <gustavoadallacroce@gmail.com>
2024-09-06 19:56:43 +0000

    cards

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,13 +4,21 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import { Navbar } from "./navbar";
 import { Jumbotron } from "./jumbotron";
+import { Cards } from "./cards";
 
 //create your first component
 const Home = () => {
+	const array = []
 	return (
 		<div className="text-center">
 			<Navbar></Navbar>
 			<Jumbotron></Jumbotron>
+			<Cards></Cards>
+			{array.map((ar1, i) => {
+				return (
+					<Cards cardsArray={ar1}></Cards>
+				)
+			})}
 		</div>
 	);
 };
--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Jumbotron = () => {
     return (
-        <div className="jumbotron text-start">
-  <h1 className="display-4">Hello, world!</h1>
-  <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        <div className="jumbotron text-start ms-3 mt-5">
+  <h1 className="display-4">A Warm Welcome!</h1>
+  <p className="lead">Lorem ipsum dolor sit amet, consectetur adipsicing elit. Ipsa, ipsam, eligendi, in quo sunt pssimus non incidunt odit vero asiquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repllat</p>
+  <a className="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
 </div>
     )
     
