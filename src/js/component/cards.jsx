33876e4f92c4ff86a5492d4f71b09813e5dd715f
nob86 <gustavoadallacroce@gmail.com>
2024-09-09 11:38:59 +0000

    falta el background del junbotrom

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Cards = (props) => {
     return(
-        <div className="card mt-5 mx-3 pt-5" style={{width: "500px", height: "500px"}}>
-  <img src="500x325" className="card-img-top" alt="..."/>
+        <div className="card mt-5 mx-3 pt-5" style={{width: "18rem"}}>
+  <img src={props.image} className="card-img-top" alt="Card image"/>
   <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipsicing elit. Ipsa, ipsam, eligendi, in quo sunt pssimus non incidunt odit vero asiquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repllat</p>
+    <h5 className="card-title">{props.title}</h5>
+    <p className="card-text">{props.text}</p>
     <a href="#" className="btn btn-primary text-center">Find out moore</a>
   </div>
 </div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,20 +8,47 @@ import { Footer } from "./footer";
 
 //create your first component
 const Home = () => {
-	const array = [1,2,3]
+	const cardsArray = [
+		{
+			title: "card 1",
+			text: "Lorem ipsum dolor sit amet, consectetur adipsicing elit. Ipsa, ipsam, eligendi, in quo sunt pssimus non incidunt odit vero asiquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repllat",
+			image: "https://via.placeholder.com/500x325"
+		},
+		{
+			title: "card 2",
+			text: "Lorem ipsum dolor sit amet, consectetur adipsicing elit. Ipsa, ipsam, eligendi, in quo sunt pssimus non incidunt odit vero asiquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repllat",
+			image: "https://via.placeholder.com/500x325"
+		},
+		{
+			title: "card 3",
+			text: "Lorem ipsum dolor sit amet, consectetur adipsicing elit. Ipsa, ipsam, eligendi, in quo sunt pssimus non incidunt odit vero asiquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repllat",
+			image: "https://via.placeholder.com/500x325"
+		},
+		{
+			title: "card 4",
+			text: "Lorem ipsum dolor sit amet, consectetur adipsicing elit. Ipsa, ipsam, eligendi, in quo sunt pssimus non incidunt odit vero asiquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repllat",
+			image: "https://via.placeholder.com/500x325"
+		},
+
+	]
 	return (
 
 		<div className="text-center">
 			<Navbar></Navbar>
 			<Jumbotron></Jumbotron>
 			
-			<div className="d-flex justify-content-center d-flex-wrap">
-			<Cards></Cards>
-			{array.map((item) => {
-				return (
-					<Cards cardsArray={item}></Cards>
+			<div className="d-flex justify-content-center flex-wrap">
+			
+			{cardsArray.map((card, index) => (
+			 (
+					<Cards
+					key={index}
+					title={card.title}
+					text={card.text}
+					image={card.image}
+					/>
 				)
-			})}
+			))}
 			</div>
 			<Footer></Footer>
 		</div>
