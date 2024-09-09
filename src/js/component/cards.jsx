0caeae602bdf42653728a2b4dae91e10de3365c4
nob86 <gustavoadallacroce@gmail.com>
2024-09-09 05:52:55 +0000

    casi terminado

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Cards = (props) => {
     return(
-        <div className="card mt-5 mx-3 pt-5" style={{width: "500px", height: "325px"}}>
+        <div className="card mt-5 mx-3 pt-5" style={{width: "500px", height: "500px"}}>
   <img src="500x325" className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,14 +4,17 @@ import React from "react";
 import { Navbar } from "./navbar";
 import { Jumbotron } from "./jumbotron";
 import { Cards } from "./cards";
+import { Footer } from "./footer";
 
 //create your first component
 const Home = () => {
 	const array = [1,2,3]
 	return (
+
 		<div className="text-center">
 			<Navbar></Navbar>
 			<Jumbotron></Jumbotron>
+			
 			<div className="d-flex justify-content-center d-flex-wrap">
 			<Cards></Cards>
 			{array.map((item) => {
@@ -20,6 +23,7 @@ const Home = () => {
 				)
 			})}
 			</div>
+			<Footer></Footer>
 		</div>
 	);
 };
